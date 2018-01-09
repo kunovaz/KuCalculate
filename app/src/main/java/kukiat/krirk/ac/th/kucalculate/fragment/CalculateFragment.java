789b4954d1ec0373b47c7c31bec6dd4a18424424
Kukiat <Kukiat@gmail.com>
2018-01-09 15:44:14 +0700

    Back Controller

--- app/src/main/java/kukiat/krirk/ac/th/kucalculate/fragment/CalculateFragment.java
+++ app/src/main/java/kukiat/krirk/ac/th/kucalculate/fragment/CalculateFragment.java
@@ -3,10 +3,12 @@ package kukiat.krirk.ac.th.kucalculate.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import kukiat.krirk.ac.th.kucalculate.MainActivity;
 import kukiat.krirk.ac.th.kucalculate.R;
 
 /**
@@ -14,6 +16,33 @@ import kukiat.krirk.ac.th.kucalculate.R;
  */
 
 public class CalculateFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+//        Create Toolbar
+        createToolbar();
+    }   // Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarCalculate);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setTitle("Calculate");
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+    }
 
     @Nullable
     @Override
